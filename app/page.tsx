967d096bc3949a01cf92e74bccfa445a87e64594
Carmen Cruzado <95239086+carcruz97@users.noreply.github.com>
2024-09-22 20:46:36 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import { useState, useEffect, useRef } from 'react'
 import { Linkedin, FileText, Twitter, Share2, Download, Github } from 'lucide-react'
 import Link from 'next/link'
